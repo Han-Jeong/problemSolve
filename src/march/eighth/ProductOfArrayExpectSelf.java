769f7e63fb0b4af8a07db10e20cbb2d05e0e550e
hann <wjdgks1233@gmail.com>
2024-03-09 13:47:08 +0900

    Product of array expect self completed

--- src/march/eighth/ProductOfArrayExpectSelf.java
+++ src/march/eighth/ProductOfArrayExpectSelf.java
@@ -5,14 +5,19 @@ import java.util.Arrays;
 public class ProductOfArrayExpectSelf {
     public int[] myAnswer(int[] nums) {
         int n = nums.length;
+        int product1 = 1;
+        int product2 = 1;
         int[] array = new int[n];
+        array[0] = 1;
 
-        Arrays.fill(array, 1);
+//        Arrays.fill(array, 1);
         for (int i = 1; i < n; i++) {
-            for (int j = i; j < n; j++) {
-                array[j - i] *= nums[j];
-                array[j] *= nums[j - i];
-            }
+            product1 *= nums[i - 1];
+            array[i] = product1;
+        }
+        for (int i = 1; i < n; i++) {
+            product2 *= nums[n - i];
+            array[n - i - 1] *= product2;
         }
         return array;
     }
